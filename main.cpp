ea77fa03fb265c0fc62e16a7d79b12e38e9b5977
RicardoLuis0 <ricolvs123@gmail.com>
2018-01-19 20:41:38 -0200

    .

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 
 int main(){
     std::string expr;
-    std::cout<<"expression to evaluate: ";
+    //std::cout<<"expression to evaluate: ";
     std::cin>>expr;
+    system("cls");
     std::cout<<std::fixed<<expr<<"="<<Eval::do_eval(expr)<<"\n";
     system("pause");
     return 0;
